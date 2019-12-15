71d9a6a9d38622d1f116f98ed7983ca7872e9310
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-16 01:11:16 +0900

    기존 App.vue, HelloWorld.vue 지우고 새로운 컴포넌트 등록하기, 그 컴포넌트로 props, event emit 구현하기

---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>{{propsdata}}</h1>
+        <button v-on:click="sendEvent">Send</button>
+    </header>
+</template>
+
+<script>
+    export default {
+        props: ['propsdata'],
+        methods: {
+            sendEvent: function () {
+                this.$emit('renew')
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
