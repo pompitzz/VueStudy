7852ff27c9aa3586e803b5868c6c5235983ef4ea
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-16 14:49:57 +0900

    TODO APP 만들기 프로젝트 생성 및 기초 틀잡기

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <TodoHeader/>
+        <TodoList/>
+        <TodoInput/>
+        <TodoFooter/>
+    </div>
+</template>
+
+<script>
+    import TodoHeader from "./components/TodoHeader";
+    import TodoList from "./components/TodoList";
+    import TodoInput from "./components/TodoInput";
+    import TodoFooter from "./components/TodoFooter";
+
+    export default {
+        components: {
+            TodoHeader,
+            TodoList,
+            TodoInput,
+            TodoFooter
+        }
+    }
+</script>
+
+<style>
+    body {
+        text-align: center;
+        background-color: #F6F6F6;
+    }
+
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+
+    button {
+        border-style: groove;
+    }
+
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+    }
+</style>
---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
+</template>
+
+<!-- scoped란 해당 컴포넌트 안에서만 유요한 스타일 속성이란 뜻이다. -->
+<style scoped>
+    h1 {
+        color: #2F3B52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
+</style>
