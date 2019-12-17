71e4b52df042636d466ad685b34bca7b51069c73
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-17 22:51:19 +0900

    뉴스 앱 만들기 프로젝트 생성 후 ESLint 설정 끄기

---
+++ vue-news/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    HELLO
+  </div>
+</template>
+
+<script>
+  // /* eslint-disable*/
+  export default {
+    methods: {
+      fetchData() {
+        console.log("wqdq");
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
