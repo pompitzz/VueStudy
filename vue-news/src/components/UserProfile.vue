a7e01dcee977ad9ac32043cb127b4e58230467a9
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-18 13:15:55 +0900

    뉴스 앱 만들기: 사용자 프로필 컴포넌트, 컴포넌트 데이터 흐름 처리 2가지 방식 이해하기

---
+++ vue-news/src/components/UserProfile.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div class="user-container">
+            <div>
+                <i class="fas fa-user"/>
+            </div>
+            <div class="user-description">
+                <div>
+                    {{getUser.id}}
+                </div>
+                <small class="created">
+                    {{getUser.created}}
+                </small>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import {mapGetters} from "vuex";
+
+    export default {
+        name: "UserProfile",
+        computed: {
+            ...mapGetters(['getUser'])
+        },
+        created() {
+            const username = this.$route.params.id;
+            this.$store.dispatch('FETCH_USER', username);
+        }
+    }
+</script>
+
+<style scoped>
+    .user-container {
+        display: flex;
+        align-items: center;
+        padding: 7px;
+    }
+
+    .fa-user {
+        font-size: 2.5rem;
+    }
+
+    .user-description {
+        padding-left: 8px;
+    }
+
+    .created {
+        font-size: 0.8rem;
+        color: #828282;
+    }
+
+</style>
--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,26 +1,24 @@
 <template>
     <div>
-        <p>name : {{getUser.id}}</p>
-        <p>karam : {{getUser.karma}}</p>
-        <p>created : {{getUser.created}}</p>
+        <!--        <user-profile :info="userInfo"/>-->
+        <user-profile/>
     </div>
 </template>
 
 <script>
+    import UserProfile from "../components/UserProfile";
     import {mapGetters} from "vuex";
 
+    /*  2가지 테이터 처리 흐름 비교해보기
+         - 1. Profile에서 store의 데이터들을 받아오기
+         - 2. props를 이용하여 여기있는 데이터를 다 Profile로 넘기기
+         (이러면 데이터 전송 flow가 한단계가 더 생긴다. 허나 데이터를 보내는 것을 명시적으로 알 수 있다.)
+     */
     export default {
         name: "UserView",
-        computed: {
-            // userInfo() {
-            //   return this.$store.state.user;
-            // },
-            ...mapGetters(['getUser'])
+        components: {
+            UserProfile,
         },
-        created() {
-            const username = this.$route.params.id;
-            this.$store.dispatch('FETCH_USER', username);
-        }
     }
 </script>
 
